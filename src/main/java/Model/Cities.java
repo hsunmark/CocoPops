34a88e85ee07a587a4279c5a7eaaee402acf204e
kabrotma <kabro@kth.se>
2016-05-03 00:51:11 +0200

    halloj

--- src/main/java/Model/Cities.java
+++ src/main/java/Model/Cities.java
@@ -3,16 +3,19 @@ package Model;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by karl-antonbrotmark on 2016-05-02.
  */
 public class Cities {
 
-    final String fileName = "src/cities.txt";
-    ArrayList<String> cities = new ArrayList<String>();
+    final String fileName = "src/airports.txt";
+    List<String> cities = new ArrayList<String>();
 
-    public String randomCity() {
+    List<String> destinationInfo = new ArrayList<String>();
+
+    public List<String> randomCity() {
 
         String currentLine;
         int randIndex = 0;
@@ -32,7 +35,40 @@ public class Cities {
             e.printStackTrace();
         }
 
-        return cities.get(randIndex);
+
+        String unformattedResult = cities.get(randIndex);
+
+        String[] dest = unformattedResult.split(", ");
+        String[] countryAndAirport = dest[1].split(" \\(");
+
+        destinationInfo.add(0,dest[0]);
+
+        //Geraldton, Australia, (GET)
+
+        destinationInfo.add(1, countryAndAirport[0]);
+
+        destinationInfo.add(2,countryAndAirport[1].substring(0,3));
+
+        // return CITY, COUNTRY, AIRPORT-CODE
+        return destinationInfo;
+
+        /**
+        * Låter regex ligga kvar för det vara så jävla jobbigt
+
+        for (int i = 0; i < cities.size(); i++) {
+            //Pattern pattern = Pattern.compile();
+            //Matcher m = pattern.matcher(cities.get(i));
+
+            if(cities.get(i).matches("\\w+, \\w+, .*?")) {
+                System.out.println(cities.get(i));
+            }
+
+
+        }
+        */
+
     }
+
+
 
 }
